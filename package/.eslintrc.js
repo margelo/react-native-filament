2cf139750de96459b28fb61ad7c59bc62fd06f63
Marc Rousavy <me@mrousavy.com>
2024-02-20 19:16:14 +0100

    Clean

---
+++ package/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  extends: [
+    "@react-native",
+    "prettier"
+  ],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        quoteProps: "consistent",
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: "es5",
+        useTabs: false
+      }
+    ]
+  }
+}
