0f8e35e183a21524abeda78841a51ec07153ae4e
Marc Rousavy <me@mrousavy.com>
2024-02-21 13:23:51 +0100

    Update build-filament.sh

--- package/scripts/build-filament.sh
+++ package/scripts/build-filament.sh
@@ -26,15 +26,14 @@ echo "Building Filament for Android ($target)"
 mkdir -p out/android-build-release-aarch64
 cd out/android-build-release-aarch64
 cmake -G Ninja \
-  -DFILAMENT_ENABLE_LTO=ON \ # Enable link-time optimizations if supported by the compiler
-  -DFILAMENT_BUILD_FILAMAT=OFF \ # Build filamat and JNI buildings
-  -DFILAMENT_SUPPORTS_OPENGL=ON \ # Include the OpenGL backend
-  -DFILAMENT_SUPPORTS_METAL=OFF \ # Include the Metal backend
-  -DFILAMENT_SUPPORTS_VULKAN=OFF \ # Include the Vulkan backend
-  -DFILAMENT_INSTALL_BACKEND_TEST=OFF \ # Install the backend test library so it can be consumed on iOS
-  -DFILAMENT_USE_EXTERNAL_GLES3=OFF \ # Experimental: Compile Filament against OpenGL ES 3
-  -DFILAMENT_USE_SWIFTSHADER=OFF \ # Compile Filament against SwiftShader
-  -DFILAMENT_SKIP_SAMPLES=ON \ # Don't build sample apps
+  -DFILAMENT_ENABLE_LTO=ON \
+  -DFILAMENT_BUILD_FILAMAT=OFF \
+  -DFILAMENT_SUPPORTS_OPENGL=ON \
+  -DFILAMENT_SUPPORTS_METAL=OFF \
+  -DFILAMENT_SUPPORTS_VULKAN=OFF \
+  -DFILAMENT_USE_EXTERNAL_GLES3=OFF \
+  -DFILAMENT_USE_SWIFTSHADER=OFF \
+  -DFILAMENT_SKIP_SAMPLES=ON \
   -DCMAKE_TOOLCHAIN_FILE=../../build/toolchain-aarch64-linux-android.cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=../android-release/filament \
