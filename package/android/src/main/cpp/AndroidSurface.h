542b336aac4b8155490c7437e7002f23dc3864b5
Marc Rousavy <me@mrousavy.com>
2024-02-21 10:25:40 +0100

    feat: Create SurfaceProvider for Android

---
+++ package/android/src/main/cpp/AndroidSurface.h
@@ -0,0 +1,35 @@
+//
+// Created by Marc Rousavy on 20.02.24.
+//
+
+#pragma once
+
+#include "Surface.h"
+#include <fbjni/fbjni.h>
+#include <jni.h>
+#include <android/native_window.h>
+
+namespace margelo {
+
+using namespace facebook;
+
+/**
+ * Implementation for the abstract class Surface, which uses the "ANativeWindow" (android.media.Surface) type underneath.
+ */
+class AndroidSurface : public Surface {
+public:
+  explicit AndroidSurface(jni::alias_ref<jobject> javaSurface);
+  ~AndroidSurface() override;
+
+  void* getSurface() override;
+  int getWidth() override;
+  int getHeight() override;
+
+  jni::global_ref<jobject> getJavaSurface();
+
+private:
+  jni::global_ref<jobject> _javaSurface;
+  ANativeWindow* _surface;
+};
+
+} // namespace margelo
---
+++ package/android/src/main/cpp/java-bindings/JSurfaceProvider.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Marc Rousavy on 21.02.24.
+//
+
+#include "JSurfaceProvider.h"
+
+namespace margelo {
+
+JSurfaceProvider::JSurfaceProvider(const jni::alias_ref<jhybridobject>& javaThis): _javaPart(make_global(javaThis)) {}
+
+JSurfaceProvider::~JSurfaceProvider() {
+    // TODO(hanno): Cleanup?
+}
+
+void JSurfaceProvider::registerNatives() {
+    registerHybrid({
+           makeNativeMethod("initHybrid", JSurfaceProvider::initHybrid),
+           makeNativeMethod("onSurfaceCreated", JSurfaceProvider::onSurfaceCreated),
+           makeNativeMethod("onSurfaceChanged", JSurfaceProvider::onSurfaceChanged),
+           makeNativeMethod("onSurfaceDestroyed", JSurfaceProvider::onSurfaceDestroyed),
+    });
+}
+
+jni::local_ref<JSurfaceProvider::jhybriddata> JSurfaceProvider::initHybrid(jni::alias_ref<jhybridobject> jThis) {
+    __android_log_write(ANDROID_LOG_INFO, TAG, "Initializing JSurfaceProvider...");
+    return makeCxxInstance(jThis);
+}
+
+void JSurfaceProvider::onSurfaceCreated(jni::alias_ref<jobject> javaSurface) {
+    if (_surface == nullptr || _surface->getJavaSurface() != javaSurface) {
+        _surface = std::make_shared<AndroidSurface>(javaSurface);
+    }
+    SurfaceProvider::onSurfaceCreated(_surface);
+}
+
+void JSurfaceProvider::onSurfaceChanged(jni::alias_ref<jobject> javaSurface, int width, int height) {
+    if (_surface == nullptr || _surface->getJavaSurface() != javaSurface) {
+        _surface = std::make_shared<AndroidSurface>(javaSurface);
+    }
+    SurfaceProvider::onSurfaceChanged(_surface, width, height);
+}
+
+void JSurfaceProvider::onSurfaceDestroyed(jni::alias_ref<jobject> surface) {
+    SurfaceProvider::onSurfaceDestroyed(_surface);
+    if (_surface != nullptr && _surface->getJavaSurface() == surface) {
+        _surface = nullptr;
+    }
+}
+
+std::shared_ptr<Surface> JSurfaceProvider::getSurfaceOrNull() {
+    return _surface;
+}
+
+} // margelo
