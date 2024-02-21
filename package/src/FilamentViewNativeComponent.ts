26426e8014f40dce41f77394f20f92667551ad75
Marc Rousavy <me@mrousavy.com>
2024-02-21 12:04:06 +0100

    feat: Link Filament submodule

--- package/src/FilamentViewNativeComponent.ts
+++ package/src/FilamentViewNativeComponent.ts
@@ -1,8 +1,8 @@
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent'
 import type { ViewProps } from 'react-native'
 
-interface NativeProps extends ViewProps {
-  color?: string
+export interface NativeProps extends ViewProps {
+  model?: number
 }
 
-export const FilamentView = codegenNativeComponent<NativeProps>('FilamentView')
+export const NativeFilamentView = codegenNativeComponent<NativeProps>('FilamentView')
