2fc03e74209cac5168716310015d3f9f064a0ee4
Marc Rousavy <me@mrousavy.com>
2024-02-21 12:46:07 +0100

    Build always for Release

--- package/scripts/build-filament.sh
+++ package/scripts/build-filament.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-mode=$1
-if [ "$mode" = 'debug' ] || [ "$mode" = 'release' ]; then
-  echo "Building Filament core library for $mode.."
-else
-  echo "Invalid parameter '$mode' - first parameter needs to be either 'debug' or 'release'!"
-  exit 1
-fi
-
 sleep 1
 cd "$(dirname "$0")"
 cd ..
 cd ..
 cd filament
 
-echo "Building Filament for iOS and Android ($mode)..."
+target="release"
+echo "Building Filament for iOS and Android ($target)..."
 # -p = platforms
 # -s = iOS simulator support
-./build.sh -p ios,android "$mode" -s
+./build.sh -p ios,android "$target" -s
 
 echo "Done!"
