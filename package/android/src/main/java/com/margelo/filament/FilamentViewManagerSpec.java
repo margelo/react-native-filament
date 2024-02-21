0c53c792fe4f0432509bda2243fda29edcd1708d
Marc Rousavy <me@mrousavy.com>
2024-02-21 21:21:14 +0100

    Properly initialize `FilamentModule`

--- package/android/src/main/java/com/margelo/filament/FilamentViewManagerSpec.java
+++ package/android/src/main/java/com/margelo/filament/FilamentViewManagerSpec.java
@@ -8,5 +8,4 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.SimpleViewManager;
 
 public abstract class FilamentViewManagerSpec<T extends View> extends SimpleViewManager<T> {
-  public abstract void setColor(T view, @Nullable String value);
 }
