d8ad12747c6504e0cd8a9eb29107e7ef331643e9
Marc Rousavy <me@mrousavy.com>
2024-02-21 15:07:22 +0100

    Create `EngineWrapper.cpp`

---
+++ package/cpp/core/EngineWrapper.h
@@ -0,0 +1,34 @@
+//
+// Created by Marc Rousavy on 21.02.24.
+//
+
+#pragma once
+
+#include <filament/Engine.h>
+#include <filament/SwapChain.h>
+#include "Surface.h"
+#include "SurfaceProvider.h"
+
+namespace margelo {
+
+using namespace filament;
+
+class EngineWrapper {
+public:
+    explicit EngineWrapper();
+    ~EngineWrapper();
+
+    void setSurfaceProvider(std::shared_ptr<SurfaceProvider> surfaceProvider);
+
+private:
+    void setSurface(std::shared_ptr<Surface> surface);
+    void destroySurface();
+
+private:
+    Engine* _engine;
+    SwapChain* _swapChain;
+    std::shared_ptr<SurfaceProvider> _surfaceProvider;
+    std::unique_ptr<Listener> _listener;
+};
+
+} // margelo
