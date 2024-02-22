43fe4b6924922a253ac105e6a8b8e52a0f5964b8
Marc Rousavy <me@mrousavy.com>
2024-02-22 11:45:59 +0100

    Test hybrid object

--- package/src/index.tsx
+++ package/src/index.tsx
@@ -1,1 +1,8 @@
+import { testHybridObject } from './test/TestHybridObject'
+
 export * from './FilamentView'
+
+const TEST_HYBRID_OBJECT = true
+if (__DEV__ && TEST_HYBRID_OBJECT) {
+  testHybridObject()
+}
