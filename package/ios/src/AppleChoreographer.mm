c91765dcf63ac1c61e48420b508861cf9150030e
Marc Rousavy <me@mrousavy.com>
2024-02-23 19:06:37 +0100

    fix: Move DisplayLinkListener to separate .m file

--- package/ios/src/AppleChoreographer.mm
+++ package/ios/src/AppleChoreographer.mm
@@ -6,33 +6,6 @@
 //
 
 #include "AppleChoreographer.h"
-#import <Foundation/Foundation.h>
-
-@implementation DisplayLinkListener {
-  CADisplayLink* _Nullable _displayLink;
-  OnFrameCallback _callback;
-}
-- (instancetype)initWithCallback:(OnFrameCallback)callback {
-  if (self = [super init]) {
-    _callback = callback;
-  }
-}
-
-- (void)start {
-  _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(onFrame:)];
-  [_displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-}
-
-- (void)stop {
-  [_displayLink invalidate];
-  _displayLink = nil;
-}
-
-- (void)onFrame:(CADisplayLink *)displayLink {
-  _callback(displayLink.timestamp);
-}
-
-@end
 
 namespace margelo {
 
---
+++ package/ios/src/DisplayLinkListener.h
@@ -0,0 +1,21 @@
+//
+//  DisplayLinkListener.h
+//  Pods
+//
+//  Created by Marc Rousavy on 23.02.24.
+//
+
+#pragma once
+
+#import <Foundation/Foundation.h>
+#import <QuartzCore/CADisplayLink.h>
+
+@interface DisplayLinkListener : NSObject
+typedef void (^ OnFrameCallback)(double timestamp);
+
+- (instancetype) initWithCallback:(OnFrameCallback)callback;
+- (void) start;
+- (void) stop;
+- (void) onFrame:(CADisplayLink*)displayLink;
+
+@end
