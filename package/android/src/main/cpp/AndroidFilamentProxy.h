dcc988b2c09b7921789e437390e2d59955114058
Marc Rousavy <me@mrousavy.com>
2024-02-24 11:47:10 +0100

    feat: Expose `getRuntime()` and `getCallInvoker()` in `FilamentProxy` (#12)

--- package/android/src/main/cpp/AndroidFilamentProxy.h
+++ package/android/src/main/cpp/AndroidFilamentProxy.h
@@ -27,6 +27,10 @@ private:
   std::shared_ptr<FilamentView> findFilamentView(int id) override;
   std::shared_ptr<Choreographer> createChoreographer() override;
 
+public:
+  jsi::Runtime& getRuntime() override;
+  std::shared_ptr<react::CallInvoker> getCallInvoker() override;
+
 private:
   jni::global_ref<JFilamentProxy::javaobject> _proxy;
 };
