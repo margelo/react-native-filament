1748d925e7258df193c2931b9e59bf55757fbc55
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-02-27 16:51:23 +0100

    fix: android iOS show the same output - almost there

--- package/cpp/core/CameraWrapper.cpp
+++ package/cpp/core/CameraWrapper.cpp
@@ -11,5 +11,5 @@ void margelo::CameraWrapper::lookAt(std::shared_ptr<ManipulatorWrapper> cameraMa
 
   math::float3 eye, center, up;
   cameraManipulator->getManipulator()->getLookAt(&eye, &center, &up);
-  _camera->lookAt({eye[0], eye[1], eye[2]}, {center[0], center[1], center[2]}, {up[0], up[1], up[2]});
+  _camera->lookAt(eye, center, up);
 }
