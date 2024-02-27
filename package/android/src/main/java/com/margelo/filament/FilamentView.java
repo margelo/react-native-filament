c72e4a1d468e6022645d0f6d45674dee8fd95afd
Marc Rousavy <me@mrousavy.com>
2024-02-27 18:06:59 +0100

    Update FilamentView.java

--- package/android/src/main/java/com/margelo/filament/FilamentView.java
+++ package/android/src/main/java/com/margelo/filament/FilamentView.java
@@ -20,27 +20,29 @@ public class FilamentView extends SurfaceView {
   @Keep
   private final HybridData mHybridData;
 
-  public void init() {
-    // Support transparent background:
-    this.setZOrderOnTop(true);
-  }
-
   public FilamentView(Context context) {
     super(context);
-    init();
     mHybridData = initHybrid(new SurfaceProvider(getHolder()));
+    setupView();
   }
 
   public FilamentView(Context context, @Nullable AttributeSet attrs) {
     super(context, attrs);
-    init();
     mHybridData = initHybrid(new SurfaceProvider(getHolder()));
+    setupView();
   }
 
   public FilamentView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
-    init();
     mHybridData = initHybrid(new SurfaceProvider(getHolder()));
+    setupView();
+  }
+
+  public void setupView() {
+    // Support transparent background
+    this.setZOrderOnTop(true);
+    // Disable overflow
+    this.setClipToOutline(true);
   }
 
   private native HybridData initHybrid(SurfaceProvider surfaceProvider);
