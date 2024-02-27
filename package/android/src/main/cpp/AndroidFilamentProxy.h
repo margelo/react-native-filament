8b5c80752e7614090c3b15f24df502e99f340417
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-02-27 13:21:37 +0100

    change loadModel -> getAssetByteBuffer

--- package/android/src/main/cpp/AndroidFilamentProxy.h
+++ package/android/src/main/cpp/AndroidFilamentProxy.h
@@ -22,7 +22,7 @@ public:
   ~AndroidFilamentProxy();
 
 private:
-  std::shared_ptr<FilamentBuffer> loadModel(std::string path) override;
+  std::shared_ptr<FilamentBuffer> getAssetByteBuffer(std::string path) override;
   std::shared_ptr<FilamentView> findFilamentView(int id) override;
   std::shared_ptr<Choreographer> createChoreographer() override;
 
