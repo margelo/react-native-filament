cedbe4143324313169c09881693c33bb44e86538
Marc Rousavy <me@mrousavy.com>
2024-02-29 10:54:24 +0100

    Use static assert if enum doesn't exist

--- package/cpp/jsi/EnumMapper.h
+++ package/cpp/jsi/EnumMapper.h
@@ -23,12 +23,26 @@ namespace EnumMapper {
                               std::to_string(static_cast<int>(passedEnum)) + ")");
   }
 
+  // Trait to check if a convertJSUnionToEnum function for enum type T exists
+  template <typename T, typename = void> struct has_js_union_to_enum : std::false_type {};
+  template <typename T>
+  struct has_js_union_to_enum<T, std::void_t<decltype(convertJSUnionToEnum(std::declval<std::string>(), std::declval<T*>()))>>
+      : std::true_type {};
+
+  // Trait to check if a convertEnumToJSUnion function for enum type T exists
+  template <typename T, typename = void> struct has_enum_to_js_union : std::false_type {};
+  template <typename T>
+  struct has_enum_to_js_union<T, std::void_t<decltype(convertEnumToJSUnion(std::declval<T>(), std::declval<std::string*>()))>>
+      : std::true_type {};
+
   template <typename TEnum> static void convertJSUnionToEnum(const std::string& inUnion, TEnum*) {
-    throw invalidUnion<TEnum>(inUnion);
+    static_assert(has_js_union_to_enum<TEnum>::value,
+                  "Cannot convert a JS union to this enum type. Did you implement EnumMapper::convertJSUnionToEnum(...)?");
   }
 
   template <typename TEnum> static void convertEnumToJSUnion(TEnum inEnum, std::string*) {
-    throw invalidEnum<TEnum>(inEnum);
+    static_assert(has_enum_to_js_union<TEnum>::value,
+                  "Cannot convert this enum type to a JS union. Did you implement EnumMapper::convertEnumToJSUnion(...)?");
   }
 } // namespace EnumMapper
 
