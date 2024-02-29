3e105b11ae7bf1c1f11a48c6271311e23f3c15cf
Marc Rousavy <me@mrousavy.com>
2024-02-29 09:47:51 +0100

    fix: Use `enum class` instead of `enum` in `TestEnum`

--- package/cpp/test/TestEnum.h
+++ package/cpp/test/TestEnum.h
@@ -8,28 +8,28 @@
 
 namespace margelo {
 
-enum TestEnum { FIRST, SECOND, THIRD };
+enum class TestEnum : uint8_t { FIRST, SECOND, THIRD };
 
 namespace EnumMapper {
   static void convertJSUnionToEnum(const std::string& inUnion, TestEnum* outEnum) {
     if (inUnion == "first")
-      *outEnum = FIRST;
+      *outEnum = TestEnum::FIRST;
     else if (inUnion == "second")
-      *outEnum = SECOND;
+      *outEnum = TestEnum::SECOND;
     else if (inUnion == "third")
-      *outEnum = THIRD;
+      *outEnum = TestEnum::THIRD;
     else
       throw invalidUnion(inUnion);
   }
   static void convertEnumToJSUnion(TestEnum inEnum, std::string* outUnion) {
     switch (inEnum) {
-      case FIRST:
+      case TestEnum::FIRST:
         *outUnion = "first";
         break;
-      case SECOND:
+      case TestEnum::SECOND:
         *outUnion = "second";
         break;
-      case THIRD:
+      case TestEnum::THIRD:
         *outUnion = "third";
         break;
       default:
