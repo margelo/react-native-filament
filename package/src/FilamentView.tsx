be4679319a27971948332b47648795270ada6e6b
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-02-29 22:41:13 +0100

    wip animations impl

--- package/src/FilamentView.tsx
+++ package/src/FilamentView.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { findNodeHandle, NativeMethods, Platform } from 'react-native'
+import { Button, findNodeHandle, NativeMethods, Platform, ScrollView, View } from 'react-native'
 import { FilamentProxy } from './native/FilamentProxy'
 import { FilamentNativeView, NativeProps } from './native/FilamentNativeView'
 import type { Float3 } from './types/float3'
@@ -19,14 +19,35 @@ const indirectLightPath = Platform.select({
   ios: 'default_env_ibl.ktx',
 })!
 
-export class FilamentView extends React.PureComponent<FilamentViewProps> {
+// Temporarily
+type _State = {
+  animationNames: string[]
+  currentAnimation: number
+}
+
+const animationInterpolationTime = 5 // 1 second
+
+export class FilamentView extends React.PureComponent<FilamentViewProps, _State> {
   private readonly ref: React.RefObject<RefType>
   private readonly engine = FilamentProxy.createEngine()
-  private animator: Animator | null = null
+  private animator: Animator
+
+  private prevAnimationIndex: number | null = null
+  private prevAnimationStarted: number | null = null
+  private animationInterpolation = 0
 
   constructor(props: FilamentViewProps) {
     super(props)
     this.ref = React.createRef<RefType>()
+
+    const pengu = this.loadPengu()
+    this.animator = pengu.getAnimator()
+    this.state = {
+      animationNames: Array.from({ length: this.animator.getAnimationCount() }, (_, i) => this.animator.getAnimationName(i)),
+      currentAnimation: 0,
+    }
+    pengu.releaseSourceData() // Cleanup memory after loading the asset
+    console.log('animationNames', this.state.animationNames)
 
     this.setup3dScene()
   }
@@ -48,17 +69,19 @@ export class FilamentView extends React.PureComponent<FilamentViewProps> {
     }, 100)
   }
 
-  setup3dScene = () => {
+  loadPengu = () => {
     // Load a model into the scene:
     const modelBuffer = FilamentProxy.getAssetByteBuffer(penguModelPath)
     const penguAsset = this.engine.loadAsset(modelBuffer)
-    this.animator = penguAsset.getAnimator()
-    penguAsset.releaseSourceData() // Cleanup memory after loading the asset
 
     // By default all assets get added to the origin at 0,0,0,
     // we transform it to fit into a unit cube at the origin using this utility:
     this.engine.transformToUnitCube(penguAsset)
 
+    return penguAsset
+  }
+
+  setup3dScene = () => {
     // Create a default light:
     const indirectLightBuffer = FilamentProxy.getAssetByteBuffer(indirectLightPath)
     this.engine.setIndirectLight(indirectLightBuffer)
@@ -74,7 +97,24 @@ export class FilamentView extends React.PureComponent<FilamentViewProps> {
     const cameraUp: Float3 = [0, 1, 0]
 
     if (this.animator) {
-      this.animator.applyAnimation(0, passedSeconds)
+      this.animator.applyAnimation(this.state.currentAnimation, passedSeconds)
+      if (this.prevAnimationIndex !== null) {
+        if (this.prevAnimationStarted === null) {
+          this.prevAnimationStarted = passedSeconds
+        }
+        this.animationInterpolation += passedSeconds - this.prevAnimationStarted
+        const alpha = this.animationInterpolation / animationInterpolationTime
+
+        this.animator.applyCrossFade(this.prevAnimationIndex, this.prevAnimationStarted!, alpha)
+        console.log('this.animationInterpolation', this.animationInterpolation)
+        console.log('passedSeconds', passedSeconds)
+        if (this.animationInterpolation >= animationInterpolationTime) {
+          this.prevAnimationIndex = null
+          this.prevAnimationStarted = null
+          this.animationInterpolation = 0
+        }
+      }
+
       this.animator.updateBoneMatrices()
     }
 
@@ -105,6 +145,23 @@ export class FilamentView extends React.PureComponent<FilamentViewProps> {
 
   /** @internal */
   public render(): React.ReactNode {
-    return <FilamentNativeView ref={this.ref} {...this.props} />
+    return (
+      <View style={this.props.style}>
+        <FilamentNativeView ref={this.ref} {...this.props} />
+
+        <ScrollView style={{ position: 'absolute', bottom: 0, maxHeight: 200, width: '100%' }}>
+          {this.state.animationNames.map((name, i) => (
+            <Button
+              key={i}
+              onPress={() => {
+                this.prevAnimationIndex = this.state.currentAnimation
+                this.setState({ currentAnimation: i })
+              }}
+              title={name}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    )
   }
 }
