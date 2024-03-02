da59124e858b245475297b3366e89883c4a1c705
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-03-02 14:54:04 +0100

    explain

--- package/scripts/BulletAndroidApplication.mk
+++ package/scripts/BulletAndroidApplication.mk
@@ -2,6 +2,7 @@ APP_MODULES := libBullet
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_OPTIM := release
 
+# Target SDK version, should be the same as in build.gradle
 APP_PLATFORM := android-31
 
 #We only need STL for placement new (#include <new>) 
