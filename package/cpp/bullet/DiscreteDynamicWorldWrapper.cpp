a3e4a93d6abfbc7e8667bedfedd3674c0c38138a
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-03-05 11:56:09 +0100

    check-all

--- package/cpp/bullet/DiscreteDynamicWorldWrapper.cpp
+++ package/cpp/bullet/DiscreteDynamicWorldWrapper.cpp
@@ -5,17 +5,17 @@
 #include "DiscreteDynamicWorldWrapper.h"
 
 namespace margelo {
-  DiscreteDynamicWorldWrapper::DiscreteDynamicWorldWrapper(double gravityX, double gravityY, double gravityZ) {
-    broadphase = std::make_unique<btDbvtBroadphase>();
+DiscreteDynamicWorldWrapper::DiscreteDynamicWorldWrapper(double gravityX, double gravityY, double gravityZ) {
+  broadphase = std::make_unique<btDbvtBroadphase>();
 
-    collisionConfiguration = std::make_unique<btDefaultCollisionConfiguration>();
-    dispatcher = std::make_unique<btCollisionDispatcher>(collisionConfiguration.get());
+  collisionConfiguration = std::make_unique<btDefaultCollisionConfiguration>();
+  dispatcher = std::make_unique<btCollisionDispatcher>(collisionConfiguration.get());
 
-    solver = std::make_unique<btSequentialImpulseConstraintSolver>();
+  solver = std::make_unique<btSequentialImpulseConstraintSolver>();
 
-    dynamicsWorld = std::make_unique<btDiscreteDynamicsWorld>(dispatcher.get(), broadphase.get(), solver.get(), collisionConfiguration.get());
-    dynamicsWorld->setGravity(btVector3(gravityX, gravityY, gravityZ));
-  }
+  dynamicsWorld = std::make_unique<btDiscreteDynamicsWorld>(dispatcher.get(), broadphase.get(), solver.get(), collisionConfiguration.get());
+  dynamicsWorld->setGravity(btVector3(gravityX, gravityY, gravityZ));
+}
 
-  void DiscreteDynamicWorldWrapper::loadHybridMethods() {}
+void DiscreteDynamicWorldWrapper::loadHybridMethods() {}
 } // namespace margelo
