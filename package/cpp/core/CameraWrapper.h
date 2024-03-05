6d91667b87295830c1d8a63bce1f814e5558c63e
Marc Rousavy <marcrousavy@hotmail.com>
2024-03-05 13:25:29 +0100

    Revert "feat: Add API for controlling the scene (#15)"
    This reverts commit c23f218aa60cfab25b60b585eb50d034b44ab4ca.

--- package/cpp/core/CameraWrapper.h
+++ package/cpp/core/CameraWrapper.h
@@ -22,11 +22,7 @@ private:
   std::shared_ptr<Camera> _camera;
 
 private:
-  void lookAt(std::vector<double> eye, std::vector<double> center, std::vector<double> up);
-  void setLensProjection(double fov, double aspect, double near, double far);
-  // TODO(Hanno): Add directionStr , Camera::Fov directionStr = Camera::Fov::VERTICAL
-  void setProjection(double fovInDegrees, double aspect, double near, double far, std::string directionStr);
   // Convenience methods
-  void lookAtCameraManipulator(std::shared_ptr<ManipulatorWrapper> cameraManipulator);
+  void lookAt(std::shared_ptr<ManipulatorWrapper> cameraManipulator);
 };
 } // namespace margelo
--- package/src/types/Engine.ts
+++ package/src/types/Engine.ts
@@ -5,49 +5,17 @@ import { Renderer } from './Renderer'
 import { Scene } from './Scene'
 import { View } from './View'
 import { FilamentBuffer } from '../native/FilamentBuffer'
-import { Entity } from './Entity'
-import { FilamentAsset } from './FilamentAsset'
-import { Float3 } from './float3'
 
 export interface Engine {
   setSurfaceProvider(surfaceProvider: SurfaceProvider): void
   setRenderCallback(callback: (engine: Engine) => void): void
 
-  /**
-   * Given a @see FilamentBuffer (e.g. from a .glb file), load the asset into the engine.
-   * This will by default add all entities from the asset to the attached default scene.
-   */
-  loadAsset(buffer: FilamentBuffer): FilamentAsset
-
-  /**
-   * Set the indirect light for the scene.
-   * @param iblBuffer A buffer containing the IBL data (e.g. from a .ktx file)
-   */
-  setIndirectLight(iblBuffer: FilamentBuffer): void
+  loadAsset(buffer: FilamentBuffer): void
+  createDefaultLight(iblBuffer: FilamentBuffer): void
 
   getRenderer(): Renderer
   getScene(): Scene
   getCamera(): Camera
   getView(): View
   getCameraManipulator(): Manipulator
-
-  createLightEntity(
-    type: 'directional' | 'spot' | 'point' | 'focused_point' | 'sun',
-    colorFahrenheit: number,
-    intensity: number,
-    directionX: number,
-    directionY: number,
-    directionZ: number,
-    castShadows: boolean,
-  ): Entity
-
-  /**
-   * Transforms the given entity to fit into a unit cube at the origin (0,0,0).
-   * @param entity The entity to transform
-   */
-  transformToUnitCube(entity: FilamentAsset): void
-
-  setEntityPosition(entity: Entity, position: Float3, multiplyCurrent: boolean): void
-  setEntityRotation(entity: Entity, angleRadians: number, axis: Float3, multiplyCurrent: boolean): void
-  setEntityScale(entity: Entity, scale: Float3, multiplyCurrent: boolean): void
 }
