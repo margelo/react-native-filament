bf5e4ba76bdf74de945af0c2a97936b59b376641
Marc Rousavy <me@mrousavy.com>
2024-03-08 18:59:48 +0100

    fix: fix all stupid bugs

--- package/src/index.tsx
+++ package/src/index.tsx
@@ -1,6 +1,12 @@
-export * from './Filament'
+// native base
 export * from './native/FilamentProxy'
 export * from './types'
+
+// react components
+export * from './Filament'
+
+// hooks
+export * from './hooks/useEngine'
 
 // import { runTests } from './test/RunTests'
 // runTests()
