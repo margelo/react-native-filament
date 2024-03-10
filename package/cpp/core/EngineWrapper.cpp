bf05cbdf45037255b3b4b0c5006f5af8c759d86a
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-03-10 12:47:53 +0100

    remove surface listener as well

--- package/cpp/core/EngineWrapper.cpp
+++ package/cpp/core/EngineWrapper.cpp
@@ -148,7 +148,7 @@ void EngineWrapper::setSurfaceProvider(std::shared_ptr<SurfaceProvider> surfaceP
                                              sharedThis->destroySurface();
                                            });
                                          }};
-  _listener = surfaceProvider->addOnSurfaceChangedListener(callback);
+  _surfaceListener = surfaceProvider->addOnSurfaceChangedListener(callback);
 }
 
 void EngineWrapper::setSurface(std::shared_ptr<Surface> surface) {
@@ -190,6 +190,7 @@ void EngineWrapper::destroySurface() {
   _choreographerListener->remove();
   _renderCallback = std::nullopt;
   _swapChain = nullptr;
+  _surfaceListener->remove();
 }
 
 void EngineWrapper::setRenderCallback(std::optional<RenderCallback> callback) {
@@ -264,9 +265,11 @@ std::shared_ptr<SwapChainWrapper> EngineWrapper::createSwapChain(std::shared_ptr
 }
 
 std::shared_ptr<CameraWrapper> EngineWrapper::createCamera() {
-  std::shared_ptr<Camera> camera = References<Camera>::adoptEngineRef(
-      _engine, _engine->createCamera(_engine->getEntityManager().create()),
-      [](const std::shared_ptr<Engine>& engine, Camera* camera) { engine->destroyCameraComponent(camera->getEntity()); });
+  std::shared_ptr<Camera> camera = References<Camera>::adoptEngineRef(_engine, _engine->createCamera(_engine->getEntityManager().create()),
+                                                                      [](const std::shared_ptr<Engine>& engine, Camera* camera) {
+                                                                        EntityManager::get().destroy(camera->getEntity());
+                                                                        engine->destroyCameraComponent(camera->getEntity());
+                                                                      });
 
   const float aperture = 16.0f;
   const float shutterSpeed = 1.0f / 125.0f;
