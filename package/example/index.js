b1d44f6d31b802a56c9fc924acea635cb0e4648e
Hanno J. Gödecke <hanno@margelo.io>
2024-03-11 16:54:33 +0100

    chore: don't expose tests in consumer API (#40)

--- package/example/index.js
+++ package/example/index.js
@@ -2,4 +2,8 @@ import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
 
+// Run filament tests
+import { runTests } from 'react-native-filament-test'
+runTests()
+
 AppRegistry.registerComponent(appName, () => App)
