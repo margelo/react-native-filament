495e21a8ca11c8fc0b8ff3cd690020ca78c60d28
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-03-11 21:07:13 +0100

    fix: don't fail if asset is null already

--- package/cpp/core/SceneWrapper.cpp
+++ package/cpp/core/SceneWrapper.cpp
@@ -25,11 +25,21 @@ void margelo::SceneWrapper::addEntity(std::shared_ptr<EntityWrapper> entity) {
 }
 
 void SceneWrapper::addAsset(std::shared_ptr<gltfio::FilamentAsset> asset) {
+  if (asset == nullptr) {
+    Logger::log("SceneWrapper", "Can't add asset an asset from scene as it was null.");
+    return;
+  }
+
   _assets.push_back(asset);
   _scene->addEntities(asset->getEntities(), asset->getEntityCount());
 }
 
 void SceneWrapper::removeAsset(std::shared_ptr<gltfio::FilamentAsset> asset) {
+  if (asset == nullptr) {
+    Logger::log("SceneWrapper", "Can't remove asset an asset from scene as it was null.");
+    return;
+  }
+
   Logger::log("SceneWrapper", "Removing an asset from scene");
   auto newEnd = std::remove(_assets.begin(), _assets.end(), asset);
   _assets.erase(newEnd, _assets.end());
