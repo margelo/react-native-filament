2faed611552366b6332d69ebec536314bba84728
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-03-11 16:47:37 +0100

    chore: remove tests from consumer index file

--- package/example/babel.config.js
+++ package/example/babel.config.js
@@ -10,6 +10,7 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js', '.json'],
         alias: {
           [pak.name]: path.join(__dirname, '..', pak.source),
+          [pak.name + '-test']: path.join(__dirname, '..', pak.test),
         },
       },
     ],
