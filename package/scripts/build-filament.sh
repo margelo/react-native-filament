707a4b36c3b22ae905ab20ae52d8dc296e2d25a2
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-03-13 11:38:17 +0100

    fix ios build issue where math headers are colliding

--- package/scripts/build-filament.sh
+++ package/scripts/build-filament.sh
@@ -27,6 +27,8 @@ echo "Copying Filament iOS libraries to react-native-filament..."
 rm -rf ../package/ios/libs/filament
 mkdir -p ../package/ios/libs/filament
 cp -rf out/ios-release/filament ../package/ios/libs
+# Rename math.h file to avoid conflicts with <math.h> system header (the header doesn't seem to be used anywhere).
+mv ../package/ios/libs/filament/include/gltfio/math.h ../package/ios/libs/filament/include/gltfio/gltfio-math.h
 
 # TODO(Marc): Use Filament from the Maven/Gradle library, to avoid shipping this huge dependency over npm.
 echo "Building Filament for Android ($target)"
