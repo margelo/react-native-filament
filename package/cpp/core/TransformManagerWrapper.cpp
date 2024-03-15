c82eb272cdef9f129357ea34a01fb4e2a488bf0d
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-03-15 21:04:09 +0100

    wip: expose transform manager

---
+++ package/cpp/core/TransformManagerWrapper.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Hanno Gödecke on 15.03.24.
+//
+
+#include "TransformManager.h"
+
+namespace margelo {} // namespace margelo
---
+++ package/cpp/core/TransformManagerWrapper.h
@@ -0,0 +1,17 @@
+//
+// Created by Hanno Gödecke on 15.03.24.
+//
+
+#pragma once
+
+//#include <filament/TransformManager.h>
+
+namespace margelo {
+
+class TransformManager : public HybridObject {
+
+private:
+
+};
+
+} // namespace margelo
