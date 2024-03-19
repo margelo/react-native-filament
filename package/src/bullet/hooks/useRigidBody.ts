d5f13b284924b76cb9b8eb528c92e715b54067be
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-03-19 12:38:25 +0100

    fix unstable dependencies

--- package/src/bullet/hooks/useRigidBody.ts
+++ package/src/bullet/hooks/useRigidBody.ts
@@ -29,25 +29,26 @@ export type RigidBodyProps = {
 export function useRigidBody(props: RigidBodyProps | undefined) {
   const originVec = props != null && 'origin' in props ? props.origin : undefined
   const transform = props != null && 'transform' in props ? props.transform : undefined
-  const { mass, shape, friction, activationState, damping, world } = props ?? {}
+  const { mass, shape, friction, activationState, damping, world, id } = props ?? {}
+
+  const originX = originVec?.[0]
+  const originY = originVec?.[1]
+  const originZ = originVec?.[2]
 
   const body = useMemo(() => {
     if (mass == null || shape == null) {
       return undefined
     }
-    if (originVec == null && transform == null) {
-      return undefined
-    }
 
-    if (originVec) {
-      return BulletAPI.createRigidBody(mass, originVec[0], originVec[1], originVec[2], shape)
+    if (originX != null && originY != null && originZ != null) {
+      return BulletAPI.createRigidBody(mass, originX, originY, originZ, shape)
     }
     if (transform) {
       return BulletAPI.createRigidBodyFromTransform(mass, transform, shape)
     }
 
     throw new Error('Either origin or transform must be provided')
-  }, [mass, originVec, shape, transform])
+  }, [mass, originX, originY, originZ, shape, transform])
 
   useEffect(() => {
     if (friction == null || body == null) {
@@ -74,7 +75,7 @@ export function useRigidBody(props: RigidBodyProps | undefined) {
     if (world == null || body == null) {
       return
     }
-    console.log('TODO: world.addRigidBody(body)')
+    console.log('world.addRigidBody(body)')
     world.addRigidBody(body)
     return () => {
       // TODO: world.removeRigidBody(body)
