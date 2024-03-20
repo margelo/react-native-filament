38d96c5e699e6202827126f19388f6ac6b87228a
Hanno J. Gödecke <hanno@margelo.io>
2024-03-20 21:49:48 +0100

    fix(android): view empty when navigating away from screen and returning (#72)

--- package/example/android/app/src/main/java/com/filamentexample/MainActivity.kt
+++ package/example/android/app/src/main/java/com/filamentexample/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.margelo.filamentexample
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +21,8 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
