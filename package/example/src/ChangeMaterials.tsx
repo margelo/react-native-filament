660c08b8f867c03086afded2d8681b38d34dcc08
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-03-22 22:23:19 +0100

    chore: release 0.11.0

--- package/example/src/ChangeMaterials.tsx
+++ package/example/src/ChangeMaterials.tsx
@@ -54,6 +54,8 @@ export function ChangeMaterials() {
       return
     }
 
+    renderableManager.setAssetEntitiesOpacity(penguAsset, 1.0)
+
     renderableManager.changeMaterialTextureMap(leftEye, 'Eye_Left.001', blueLeftEyeBuffer, 'sRGB')
     renderableManager.changeMaterialTextureMap(rightEye, 'Eye_Right.002', blueRightEyeBuffer, 'sRGB')
   }, [blueLeftEyeBuffer, blueRightEyeBuffer, engine, penguAsset, renderableManager])
