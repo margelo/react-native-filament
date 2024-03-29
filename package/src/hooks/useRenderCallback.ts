f972bf88836f63c09cd2741b1bdb6cdb6f77cfa9
Marc Rousavy <me@mrousavy.com>
2024-03-29 14:02:28 +0100

    try to make Render Callback use Worklets

--- package/src/hooks/useRenderCallback.ts
+++ package/src/hooks/useRenderCallback.ts
@@ -1,23 +1,29 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import type { Engine, RenderCallback } from '../types'
-import { reportError } from '../ErrorUtils'
+import { reportWorkletError } from '../ErrorUtils'
+import { FilamentProxy } from '../native/FilamentProxy'
+import { useSharedValue } from 'react-native-worklets-core'
 
 export function useRenderCallback(engine: Engine, onFrame: RenderCallback) {
-  const renderCallback = useRef(onFrame)
-  renderCallback.current = onFrame
+  const renderCallback = useSharedValue(onFrame)
+  renderCallback.value = onFrame
 
   useEffect(() => {
+    const context = FilamentProxy.getWorkletContext()
     // configure a render callback when this hook mounts
-    engine.setRenderCallback((...args) => {
-      try {
-        renderCallback.current(...args)
-      } catch (e) {
-        reportError(e)
-      }
-    })
+    Worklets.createRunInContextFn(() => {
+      'worklet'
+      engine.setRenderCallback((...args) => {
+        try {
+          renderCallback.value(...args)
+        } catch (e) {
+          reportWorkletError(e)
+        }
+      })
+    }, context)()
     return () => {
       // remove render callback on unmount
       engine.setRenderCallback(undefined)
     }
-  }, [engine])
+  }, [engine, renderCallback])
 }
