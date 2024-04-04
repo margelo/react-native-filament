b6226e05a6ce7615dc70d31972ed6b0034c4e63a
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-04 09:14:14 +0200

    feat: added useWorkletCallback

---
+++ package/src/hooks/useWorkletCallback.ts
@@ -0,0 +1,16 @@
+import { DependencyList, useMemo } from 'react'
+import { useWorklet } from 'react-native-worklets-core'
+import { FilamentProxy } from '../native/FilamentProxy'
+
+/**
+ * Creates a callback that can be executed in he separate worklet thread of the engine.
+ */
+export function useWorkletCallback<T extends (...args: any[]) => any>(
+  callback: T,
+  deps: DependencyList
+): (...args: Parameters<T>) => Promise<ReturnType<T>> {
+  const workletContext = useMemo(() => FilamentProxy.getWorkletContext(), [])
+
+  const workletCallback = useWorklet<ReturnType<T>, Parameters<T>, T>(workletContext, callback, deps)
+  return workletCallback
+}
