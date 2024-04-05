1a9b6c3eed12029ad61d9067fb231a42f7af2146
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-05 20:33:40 +0200

    fix: export BulletAPI

--- package/src/bullet/bulletApi.ts
+++ package/src/bullet/bulletApi.ts
@@ -1,3 +1,7 @@
 import { FilamentProxy } from '../native/FilamentProxy'
 
+/**
+ * Note: Prefer using the bullet hooks over the imperative API!
+ * The imperative API might become deprecated in the future.
+ */
 export const BulletAPI = FilamentProxy.createBullet()
