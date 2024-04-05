ff746d729fdb63036cd693928f52031122a21525
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-05 15:17:17 +0200

    fix: crash when asset gets destroyed but animator instance is still active
    Keep a reference on the asset in the animator.
    This way the asset won't be destroyed when the animator is still being used

--- package/cpp/core/AnimatorWrapper.h
+++ package/cpp/core/AnimatorWrapper.h
@@ -14,7 +14,8 @@ using namespace filament::gltfio;
 class AnimatorWrapper : public HybridObject {
 public:
   explicit AnimatorWrapper(const std::shared_ptr<FilamentAsset>& asset)
-      : HybridObject("AnimatorWrapper"), _instance(asset->getInstance()) {}
+      : HybridObject("AnimatorWrapper"), _assetRef(asset), _instance(asset->getInstance()) {}
+
   // TODO(copy-animations): We currently copy animations from an asset onto another instance (different model than the original asset), we
   // should replace this with once we found a good solution discussed here: https://github.com/google/filament/issues/7622
   explicit AnimatorWrapper(const std::shared_ptr<FilamentAsset>& asset, Animator* optionalAnimator) : AnimatorWrapper(asset) {
@@ -37,6 +38,8 @@ private:
   Animator* getAnimator();
 
 private:
+  // Keep a reference on the asset to avoid the animator instance being alive while the asset got already deallocated
+  std::shared_ptr<FilamentAsset> _assetRef = nullptr;
   FilamentInstance* _instance;
   // TODO(copy-animations): We currently copy animations from an asset onto another instance (different model than the original asset), we
   // should replace this with once we found a good solution discussed here: https://github.com/google/filament/issues/7622
