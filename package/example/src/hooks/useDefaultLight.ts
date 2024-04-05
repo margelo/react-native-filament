64060b661bc5756c178e8d139fa823eabd984d46
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-05 09:40:49 +0200

    chore(example): fix useDefault light to cleanup

--- package/example/src/hooks/useDefaultLight.ts
+++ package/example/src/hooks/useDefaultLight.ts
@@ -1,5 +1,6 @@
+import { useEffect } from 'react'
 import { Platform } from 'react-native'
-import { Engine, useAsset, useScene, useWorkletEffect } from 'react-native-filament'
+import { Engine, useAsset, useScene } from 'react-native-filament'
 
 const indirectLightPath = Platform.select({
   android: 'custom/default_env_ibl.ktx',
@@ -10,11 +11,14 @@ export function useDefaultLight(engine: Engine) {
   const lightBuffer = useAsset({ path: indirectLightPath })
   const scene = useScene(engine)
 
-  useWorkletEffect(() => {
-    'worklet'
+  useEffect(() => {
     if (lightBuffer == null) return
     engine.setIndirectLight(lightBuffer)
     const directionalLight = engine.createLightEntity('directional', 6500, 10000, 0, -1, 0, true)
     scene.addEntity(directionalLight)
-  }, [lightBuffer, scene])
+
+    return () => {
+      scene.removeEntity(directionalLight)
+    }
+  }, [engine, lightBuffer, scene])
 }
