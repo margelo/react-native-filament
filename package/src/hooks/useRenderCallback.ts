ddd473e3b039fb107c4c9bec36aa51a04c1dc2ac
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-08 13:03:09 +0200

    fix: render callback declare as worklet

--- package/src/hooks/useRenderCallback.ts
+++ package/src/hooks/useRenderCallback.ts
@@ -11,6 +11,8 @@ export function useRenderCallback(engine: Engine, onFrame: RenderCallback) {
     Worklets.createRunInContextFn(() => {
       'worklet'
       engine.setRenderCallback((...args) => {
+        'worklet'
+
         // TODO: Right now the worklet context isn't deallocated when we reload the app.
         // If you remove all the code below (so just pass an empty worklet function to setRenderCallback),
         // everything gets cleaned up properly.
