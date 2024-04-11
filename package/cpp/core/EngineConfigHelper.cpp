c712b4a9239f39b54cb7db5e2cbf536cc1c10b21
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-11 09:43:17 +0200

    wip: add helper for config

---
+++ package/cpp/core/EngineConfigHelper.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Hanno Gödecke on 11.04.24.
+//
+
+#include "EngineConfigHelper.h"
+
+namespace margelo {
+} // margelo
---
+++ package/cpp/core/EngineConfigHelper.h
@@ -0,0 +1,16 @@
+//
+// Created by Hanno Gödecke on 11.04.24.
+//
+
+#ifndef FILAMENTEXAMPLE_ENGINECONFIGHELPER_H
+#define FILAMENTEXAMPLE_ENGINECONFIGHELPER_H
+
+namespace margelo {
+
+class EngineConfigHelper {
+
+};
+
+} // margelo
+
+#endif //FILAMENTEXAMPLE_ENGINECONFIGHELPER_H
