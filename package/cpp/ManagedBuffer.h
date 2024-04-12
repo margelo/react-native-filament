4f108bb5f88d6395ff77e184a3c7c7212dd54b2e
Hanno J. Gödecke <hanno@margelo.io>
2024-04-12 18:24:59 +0200

    perf: release asset buffers once assets are loaded (#106)

--- package/cpp/ManagedBuffer.h
+++ package/cpp/ManagedBuffer.h
@@ -14,6 +14,11 @@ public:
   virtual ~ManagedBuffer() {}
   virtual const uint8_t* getData() const = 0;
   virtual size_t getSize() const = 0;
+  // Note: in general we don't want to manually release anything.
+  // However, the reference to the Buffers is managed on the JS side and Hades seldomly
+  // GCs these buffers as it can't see their true size. Thus we need a mechanism to
+  // manually release the memory of the buffers once we have loaded an asset.
+  virtual void release() {}
 };
 
 } // namespace margelo
--- package/src/native/FilamentBuffer.ts
+++ package/src/native/FilamentBuffer.ts
@@ -1,3 +1,8 @@
-export interface FilamentBuffer {}
+export interface FilamentBuffer {
+  /**
+   * Internal method to release memory early when the buffer is no longer needed.
+   */
+  release(): void
+}
 
 export type Asset = FilamentBuffer
