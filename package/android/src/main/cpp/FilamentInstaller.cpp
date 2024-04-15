b4eb1405a7e51f99b3b672ad231be532f21435de
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-15 10:35:04 +0200

    check-all

--- package/android/src/main/cpp/FilamentInstaller.cpp
+++ package/android/src/main/cpp/FilamentInstaller.cpp
@@ -8,29 +8,21 @@
 
 namespace margelo {
 
-void FilamentInstaller::install(
-    jni::alias_ref<jni::JClass> clazz,
-    jni::alias_ref<JFilamentProxy::javaobject> proxy) {
-  jsi::Runtime &runtime = proxy->cthis()->getRuntime();
-  const std::shared_ptr<react::CallInvoker> &callInvoker =
-      proxy->cthis()->getCallInvoker();
+void FilamentInstaller::install(jni::alias_ref<jni::JClass> clazz, jni::alias_ref<JFilamentProxy::javaobject> proxy) {
+  jsi::Runtime& runtime = proxy->cthis()->getRuntime();
+  const std::shared_ptr<react::CallInvoker>& callInvoker = proxy->cthis()->getCallInvoker();
 
   // global.FilamentProxy
-  std::shared_ptr<AndroidFilamentProxy> filamentProxy =
-      std::make_shared<AndroidFilamentProxy>(proxy);
-  runtime.global().setProperty(
-      runtime, "FilamentProxy",
-      jsi::Object::createFromHostObject(runtime, filamentProxy));
+  std::shared_ptr<AndroidFilamentProxy> filamentProxy = std::make_shared<AndroidFilamentProxy>(proxy);
+  runtime.global().setProperty(runtime, "FilamentProxy", jsi::Object::createFromHostObject(runtime, filamentProxy));
 
   // PromiseFactory
-  std::shared_ptr<Dispatcher> jsDispatcher =
-      std::make_shared<CallInvokerDispatcher>(callInvoker);
+  std::shared_ptr<Dispatcher> jsDispatcher = std::make_shared<CallInvokerDispatcher>(callInvoker);
   margelo::PromiseFactory::install(runtime, jsDispatcher);
 }
 
 void FilamentInstaller::registerNatives() {
-  javaClassStatic()->registerNatives(
-      {makeNativeMethod("install", FilamentInstaller::install)});
+  javaClassStatic()->registerNatives({makeNativeMethod("install", FilamentInstaller::install)});
 }
 
 } // namespace margelo
