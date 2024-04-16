1e88d91f3857dbd1524ea00bcf14104899bc9a77
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-16 19:53:49 +0200

    fix: useLightEntity not memoizing properly

--- package/src/hooks/useLightEntity.ts
+++ package/src/hooks/useLightEntity.ts
@@ -17,27 +17,45 @@ export function useLightEntity(
   const falloffRadius = 'falloffRadius' in config ? config.falloffRadius : undefined
   const spotLightCone = 'spotLightCone' in config ? config.spotLightCone : undefined
 
+  // Unwrap all array-ish values so the user doesn't have to memo them!
+  const directionX = config.direction?.[0] ?? undefined
+  const directionY = config.direction?.[1] ?? undefined
+  const directionZ = config.direction?.[2] ?? undefined
+
+  const positionX = config.position?.[0] ?? undefined
+  const positionY = config.position?.[1] ?? undefined
+  const positionZ = config.position?.[2] ?? undefined
+
+  const innerSpotLightCone = spotLightCone?.[0] ?? undefined
+  const outerSpotLightCone = spotLightCone?.[1] ?? undefined
+
   const entity = useMemo(() => {
+    console.log('Creating light entity')
     return lightManager.createLightEntity(
       config.type,
       config.colorKelvin,
       typeof config.intensity === 'number' ? config.intensity : config.intensity?.value,
-      config.direction,
-      config.position,
+      directionX != null && directionY != null && directionZ != null ? [directionX, directionY, directionZ] : undefined,
+      positionX != null && positionY != null && positionZ != null ? [positionX, positionY, positionZ] : undefined,
       config.castShadows,
       falloffRadius,
-      spotLightCone
+      innerSpotLightCone != null && outerSpotLightCone != null ? [innerSpotLightCone, outerSpotLightCone] : undefined
     )
   }, [
     config.castShadows,
     config.colorKelvin,
-    config.direction,
     config.intensity,
-    config.position,
     config.type,
+    directionX,
+    directionY,
+    directionZ,
     falloffRadius,
+    innerSpotLightCone,
     lightManager,
-    spotLightCone,
+    outerSpotLightCone,
+    positionX,
+    positionY,
+    positionZ,
   ])
 
   // Eventually subscribe to the intensity shared value
@@ -54,7 +72,7 @@ export function useLightEntity(
         setIntensity(entity, intensity.value)
       })
     )
-  }, [config.intensity, entity, lightManager.setIntensity])
+  }, [config.intensity, entity, lightManager])
 
   return entity
 }
