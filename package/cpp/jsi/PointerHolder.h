14d375d460af52ca4cff854f177c1e6daa630c67
Marc Rousavy <me@mrousavy.com>
2024-04-16 15:21:45 +0200

    Add docs

--- package/cpp/jsi/PointerHolder.h
+++ package/cpp/jsi/PointerHolder.h
@@ -16,14 +16,37 @@ using namespace facebook;
 template <typename T>
 class PointerHolder: public HybridObject {
  protected:
+  // no default constructor
   PointerHolder() = delete;
-  PointerHolder(const char* name, std::shared_ptr<T> pointer): HybridObject(name), _name(name), _pointer(pointer) { }
+
+  /**
+   * Create a new instance of a pointer holder which holds the given shared_ptr.
+   * @param name The name of the implementing class, for example "ViewWrapper".
+   * @param pointer The pointer this class will hold. It might be released from JS at any point via `release()`.
+   */
+  PointerHolder(const char* name, std::shared_ptr<T> pointer): HybridObject(name), _name(name), _pointer(pointer) {
+    // eagerly initialize the release() method instead of putting it in `loadHybridMethods`
+    registerHybridMethod("release", &PointerHolder<T>::release, this);
+  }
+  /**
+   * Create a new instance of a pointer holder which holds a shared_ptr of the given value.
+   * The shared_ptr will be move-constructed.
+   * @param name The name of the implementing class, for example "ViewWrapper".
+   * @param value The value this class will hold as a shared_ptr. It might be destroyed from JS at any point via `release()`.
+   */
   PointerHolder(const char* name, T&& value): PointerHolder(name, std::make_shared<T>(std::move(value))) { }
+  /**
+   * Called when the PointerHolder gets automatically destroyed (e.g. via GC) and the shared_ptr will be destroyed.
+   */
   ~PointerHolder() {
     Logger::log(TAG, "Automatically releasing %s... (~PointerHolder())", _name);
   }
 
 protected:
+  /**
+   * Manually release this reference to the pointer.
+   * If there are any other references to this pointer, no memory will be force-deleted.
+   */
   void release() {
     std::unique_lock lock(_mutex);
 
@@ -34,6 +57,10 @@ protected:
     _pointer = nullptr;
   }
 
+  /**
+   * Get the shared_ptr this class is holding.
+   * If it has already been manually released from JS, this method will throw a runtime_error.
+   */
   std::shared_ptr<T> pointee() {
     std::unique_lock lock(_mutex);
 
