5928d3f56b3c2315b608c356534292d99a4c8f91
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-25 21:41:02 +0200

    format + revert bogus code

--- package/ios/src/DisplayLinkListener.m
+++ package/ios/src/DisplayLinkListener.m
@@ -12,18 +12,24 @@
 @implementation DisplayLinkListener {
   CADisplayLink* _Nullable _displayLink;
   OnFrameCallback _callback;
-    bool shouldContinueOnEnterForeground;
+  bool shouldContinueOnEnterForeground;
 }
 - (instancetype)initWithCallback:(OnFrameCallback)callback {
   if (self = [super init]) {
     _callback = callback;
     _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(onFrame:)];
     _displayLink.paused = YES;
-      shouldContinueOnEnterForeground = NO;
+    shouldContinueOnEnterForeground = NO;
     [_displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-      
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidEnterBackground) name:UIApplicationDidEnterBackgroundNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appWillEnterForeground) name:UIApplicationWillEnterForegroundNotification object:nil];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appDidEnterBackground)
+                                                 name:UIApplicationDidEnterBackgroundNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appWillEnterForeground)
+                                                 name:UIApplicationWillEnterForegroundNotification
+                                               object:nil];
   }
   return self;
 }
@@ -33,7 +39,7 @@
 }
 
 - (void)stop {
-  _displayLink.paused = NO;
+  _displayLink.paused = YES;
 }
 
 - (void)invalidate {
@@ -49,16 +55,16 @@
 // When the app enters the background we need to stop sending commands to metal.
 // Otherwise we run the risk of the metal view becoming blank, or not being able to update it anymore.
 - (void)appDidEnterBackground {
-    shouldContinueOnEnterForeground = !_displayLink.paused;
-    _displayLink.paused = YES;
-    NSLog(@"[react-native-filament] [DisplayLinkListener] Paused rendering, app entered background.");
+  shouldContinueOnEnterForeground = !_displayLink.paused;
+  _displayLink.paused = YES;
+  NSLog(@"[react-native-filament] [DisplayLinkListener] Paused rendering, app entered background.");
 }
 
 - (void)appWillEnterForeground {
-    if (shouldContinueOnEnterForeground){
-        _displayLink.paused = NO;
-        NSLog(@"[react-native-filament] [DisplayLinkListener] Resumed rendering, app entered foreground.");
-    }
+  if (shouldContinueOnEnterForeground) {
+    _displayLink.paused = NO;
+    NSLog(@"[react-native-filament] [DisplayLinkListener] Resumed rendering, app entered foreground.");
+  }
 }
 
 - (void)dealloc {
