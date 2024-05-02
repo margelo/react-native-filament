9f95c581285f5f39b88c933000f7fe084be52b92
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-05-02 15:30:10 +0200

    check-all

--- package/android/src/main/cpp/AndroidFilamentProxy.h
+++ package/android/src/main/cpp/AndroidFilamentProxy.h
@@ -19,12 +19,11 @@ using namespace facebook;
  */
 class AndroidFilamentProxy : public FilamentProxy {
 public:
-  explicit AndroidFilamentProxy(
-      jni::alias_ref<JFilamentProxy::javaobject> filamentProxy);
+  explicit AndroidFilamentProxy(jni::alias_ref<JFilamentProxy::javaobject> filamentProxy);
   ~AndroidFilamentProxy();
 
 private:
-  std::shared_ptr<FilamentBuffer> loadAsset(const std::string &path) override;
+  std::shared_ptr<FilamentBuffer> loadAsset(const std::string& path) override;
   std::shared_ptr<FilamentView> findFilamentView(int id) override;
   std::shared_ptr<Choreographer> createChoreographer() override;
   std::shared_ptr<Dispatcher> getRenderThreadDispatcher() override;
@@ -34,7 +33,7 @@ private:
   float getDensityPixelRatio() override;
 
 public:
-  jsi::Runtime &getRuntime() override;
+  jsi::Runtime& getRuntime() override;
   std::shared_ptr<react::CallInvoker> getCallInvoker() override;
 
 private:
