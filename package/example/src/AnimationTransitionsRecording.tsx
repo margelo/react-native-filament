9d82aeb413c9291a1588cb3a78acfc106f20490a
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-05-03 13:10:51 +0200

    wip: recording example

---
+++ package/example/src/AnimationTransitionsRecording.tsx
@@ -0,0 +1,106 @@
+import * as React from 'react'
+import { useCallback } from 'react'
+import { StyleSheet, View } from 'react-native'
+import {
+  Filament,
+  Float3,
+  useModel,
+  useAssetAnimator,
+  getAssetFromModel,
+  FilamentProvider,
+  useFilamentContext,
+  useResource,
+  useSkybox,
+  RenderCallback,
+} from 'react-native-filament'
+import { useDefaultLight } from './hooks/useDefaultLight'
+import { getAssetPath } from './utils/getAssetPasth'
+import { useSharedValue } from 'react-native-worklets-core'
+
+const penguModelPath = getAssetPath('pengu.glb')
+const pirateHatPath = getAssetPath('pirate.glb')
+
+// Camera config:
+const cameraPosition: Float3 = [0, 0, 8]
+const cameraTarget: Float3 = [0, 0, 0]
+const cameraUp: Float3 = [0, 1, 0]
+const focalLengthInMillimeters = 28
+const near = 0.1
+const far = 1000
+
+function Renderer() {
+  const { camera, view } = useFilamentContext()
+  useDefaultLight()
+  useSkybox({ color: '#88defb' })
+
+  const pengu = useModel({ path: penguModelPath })
+  const penguAsset = getAssetFromModel(pengu)
+  const pirateHat = useModel({ path: pirateHatPath })
+  const pirateHatAsset = getAssetFromModel(pirateHat)
+  const pirateHatAnimator = useResource(() => {
+    if (pirateHatAsset == null || penguAsset == null) {
+      return undefined
+    }
+    return Promise.resolve(pirateHatAsset.createAnimatorWithAnimationsFrom(penguAsset))
+  }, [pirateHatAsset, penguAsset])
+
+  const penguAnimator = useAssetAnimator(penguAsset)
+
+  const prevAspectRatio = useSharedValue(0)
+  const renderCallback: RenderCallback = useCallback(
+    ({ passedSeconds }) => {
+      'worklet'
+
+      const aspectRatio = view.getAspectRatio()
+      if (prevAspectRatio.value !== aspectRatio) {
+        prevAspectRatio.value = aspectRatio
+        // Setup camera lens:
+        camera.setLensProjection(focalLengthInMillimeters, aspectRatio, near, far)
+        console.log('Setting up camera lens with aspect ratio:', aspectRatio)
+      }
+
+      camera.lookAt(cameraPosition, cameraTarget, cameraUp)
+
+      if (pirateHatAnimator == null || penguAnimator == null) {
+        return
+      }
+
+      // Update the animators to play the current animation
+      penguAnimator.applyAnimation(0, passedSeconds)
+      pirateHatAnimator.applyAnimation(0, passedSeconds)
+
+      penguAnimator.updateBoneMatrices()
+      pirateHatAnimator.updateBoneMatrices()
+    },
+    [view, prevAspectRatio, camera, pirateHatAnimator, penguAnimator]
+  )
+
+  return (
+    <View style={styles.container}>
+      <Filament style={styles.filamentView} enableTransparentRendering={false} renderCallback={renderCallback} />
+    </View>
+  )
+}
+
+export function AnimationTransitionsRecording() {
+  return (
+    <FilamentProvider>
+      <Renderer />
+    </FilamentProvider>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  filamentView: {
+    flex: 1,
+  },
+  btnContainer: {
+    height: 200,
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+  },
+})
