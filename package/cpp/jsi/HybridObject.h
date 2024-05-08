5220f7d826facc7fc0b79a00e271f89cc18a85e3
Marc Rousavy <me@mrousavy.com>
2024-05-08 22:30:39 +0200

    feat: Allow full JSI control in `HybridObject`

--- package/cpp/jsi/HybridObject.h
+++ package/cpp/jsi/HybridObject.h
@@ -78,7 +78,7 @@ private:
 
 private:
   template <typename Derived, typename ReturnType, typename... Args, size_t... Is>
-  inline jsi::Value callMethod(Derived* obj, ReturnType (Derived::*method)(Args...), jsi::Runtime& runtime, const jsi::Value* args,
+  static inline jsi::Value callMethod(Derived* obj, ReturnType (Derived::*method)(Args...), jsi::Runtime& runtime, const jsi::Value* args,
                                std::index_sequence<Is...>) {
     if constexpr (std::is_same_v<ReturnType, void>) {
       // It's a void method.
@@ -92,12 +92,21 @@ private:
   }
 
   template <typename Derived, typename ReturnType, typename... Args>
-  jsi::HostFunctionType createHybridMethod(ReturnType (Derived::*method)(Args...), Derived* derivedInstance) {
-    return [this, derivedInstance, method](jsi::Runtime& runtime, const jsi::Value& thisVal, const jsi::Value* args,
-                                           size_t count) -> jsi::Value {
-      // Call the actual method with JSI values as arguments and return a JSI value again.
-      // Internally, this method converts the JSI values to C++ values.
-      return callMethod(derivedInstance, method, runtime, args, std::index_sequence_for<Args...>{});
+  static jsi::HostFunctionType createHybridMethod(ReturnType (Derived::*method)(Args...), Derived* derivedInstance) {
+
+    return [derivedInstance, method](jsi::Runtime &runtime, const jsi::Value &thisVal,
+                                     const jsi::Value *args,
+                                     size_t count) -> jsi::Value {
+      if constexpr (std::is_same_v<ReturnType, jsi::Value>) {
+        // If the return type is a jsi::Value, we assume the user wants full JSI code control.
+        // The signature must be identical to jsi::HostFunction (jsi::Runtime&, jsi::Value& this, ...)
+        return (derivedInstance->*method)(runtime, thisVal, args, count);
+      } else {
+        // Call the actual method with JSI values as arguments and return a JSI value again.
+        // Internally, this method converts the JSI values to C++ values.
+        return callMethod(derivedInstance, method, runtime, args,
+                          std::index_sequence_for<Args...>{});
+      }
     };
   }
 
