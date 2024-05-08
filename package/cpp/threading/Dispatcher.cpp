1b5ca8eda65105c01563f94de86b66ee65055336
Marc Rousavy <me@mrousavy.com>
2024-05-08 22:48:50 +0200

    Rename

--- package/cpp/threading/Dispatcher.cpp
+++ package/cpp/threading/Dispatcher.cpp
@@ -6,13 +6,13 @@ namespace margelo {
 
 using namespace facebook;
 
-static constexpr auto GLOBAL_PROMISE_FACTORY_DISPATCHER_HOLDER_NAME = "__promiseFactoryDispatcher";
+static constexpr auto GLOBAL_DISPATCHER_HOLDER_NAME = "__globalDispatcher";
 
 void Dispatcher::installRuntimeGlobalDispatcher(jsi::Runtime& runtime, std::shared_ptr<Dispatcher> dispatcher) {
   Logger::log(TAG, "Installing global Dispatcher Holder...");
 
   auto dispatcherHolder = jsi::Object::createFromHostObject(runtime, dispatcher);
-  runtime.global().setProperty(runtime, GLOBAL_PROMISE_FACTORY_DISPATCHER_HOLDER_NAME, dispatcherHolder);
+  runtime.global().setProperty(runtime, GLOBAL_DISPATCHER_HOLDER_NAME, dispatcherHolder);
 }
 
 std::shared_ptr<Dispatcher> Dispatcher::getRuntimeGlobalDispatcher(jsi::Runtime& runtime) {
@@ -23,14 +23,14 @@ std::shared_ptr<Dispatcher> Dispatcher::getRuntimeGlobalDispatcher(jsi::Runtime&
 
 jsi::Value Dispatcher::getRuntimeGlobalDispatcherHolder(jsi::Runtime& runtime) {
 #if DEBUG
-  if (!runtime.global().hasProperty(runtime, GLOBAL_PROMISE_FACTORY_DISPATCHER_HOLDER_NAME)) {
+  if (!runtime.global().hasProperty(runtime, GLOBAL_DISPATCHER_HOLDER_NAME)) {
     throw jsi::JSError(runtime, "Failed to get current Dispatcher - the global Dispatcher holder does not exist! "
                                 "Was Dispatcher::installDispatcherIntoRuntime() called for this jsi::Runtime? "
                                 "(global." +
-                                    std::string(GLOBAL_PROMISE_FACTORY_DISPATCHER_HOLDER_NAME) + " == undefined)");
+                                    std::string(GLOBAL_DISPATCHER_HOLDER_NAME) + " == undefined)");
   }
 #endif
-  return runtime.global().getProperty(runtime, GLOBAL_PROMISE_FACTORY_DISPATCHER_HOLDER_NAME);
+  return runtime.global().getProperty(runtime, GLOBAL_DISPATCHER_HOLDER_NAME);
 }
 
 } // namespace margelo
