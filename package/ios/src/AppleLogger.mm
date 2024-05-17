39e97c4fd0f8366dc3ca03588fc155ba4eef15cd
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-05-17 15:13:56 +0200

    fi ios don't show logs

--- package/ios/src/AppleLogger.mm
+++ package/ios/src/AppleLogger.mm
@@ -11,10 +11,12 @@
 namespace margelo {
 
 void Logger::log(const std::string& tag, const std::string& message) {
+#if RNF_ENABLE_LOGS
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wformat-security"
   NSLog(@"[RNF/%s]: %s", tag.c_str(), message.c_str());
 #pragma clang diagnostic pop
+#endif
 }
 
 } // namespace margelo
