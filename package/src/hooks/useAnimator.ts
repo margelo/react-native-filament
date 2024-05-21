67a68ac2f961364a7b09e7a8a4dfb67945025339
Hanno J. Gödecke <hanno@margelo.io>
2024-05-21 13:10:38 +0200

    fix: fix crash in useAnimator (#188)

--- package/src/hooks/useAnimator.ts
+++ package/src/hooks/useAnimator.ts
@@ -3,22 +3,28 @@ import { Animator, FilamentAsset, FilamentInstance } from '../types'
 import { useFilamentContext } from '../FilamentContext'
 import { FilamentModel } from './useModel'
 
+function isFilamentModel(asset: FilamentAsset | FilamentInstance | FilamentModel): asset is FilamentModel {
+  // @ts-expect-error Because asset is a HostObject and using "in" check is a) expensive and b) somehow broken
+  return asset.state != null
+}
+
 /**
  * Creates a animator for the given {@linkcode FilamentAsset} or {@linkcode FilamentInstance}.
  *
  * @note For each asset/instance you should only have one animator.
  */
-export function useAnimator(asset?: FilamentAsset | FilamentInstance | FilamentModel): Animator | undefined {
+export function useAnimator(modelOrAsset?: FilamentAsset | FilamentInstance | FilamentModel): Animator | undefined {
   const { nameComponentManager } = useFilamentContext()
   const animator = useMemo(() => {
-    if (asset == null) return undefined
-    if ('state' in asset) {
-      if (asset.state === 'loading') return undefined
-      const modelAsset = asset.asset
-      return modelAsset.createAnimator(nameComponentManager)
+    if (modelOrAsset == null) return undefined
+
+    if (isFilamentModel(modelOrAsset)) {
+      if (modelOrAsset.state === 'loading') return undefined
+      const { asset } = modelOrAsset
+      return asset.createAnimator(nameComponentManager)
     }
 
-    return asset.createAnimator(nameComponentManager)
-  }, [asset, nameComponentManager])
+    return modelOrAsset.createAnimator(nameComponentManager)
+  }, [modelOrAsset, nameComponentManager])
   return animator
 }
