00413f9f0335bdd96fd0daae32ff8c5a89fe5768
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-05-28 17:44:17 +0200

    wip: setup FabricExample app which works with android

--- package/example/react-native.config.js
+++ package/example/react-native.config.js
@@ -6,6 +6,9 @@ module.exports = {
     [pak.name]: {
       root: path.join(__dirname, '..'),
     },
+    ['react-native-worklets-core']: {
+      root: path.join(__dirname, 'node_modules', 'react-native-worklets-core'),
+    },
   },
   assets: ['./assets'],
 }
