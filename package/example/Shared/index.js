a257927a7db09eb14f5ac3ff275e626546d0b1d5
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-05-31 15:48:49 +0200

    enable strict mode again

--- package/example/Shared/index.js
+++ package/example/Shared/index.js
@@ -1,10 +1,14 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 
 // Run filament tests
 import { runTests } from 'react-native-filament-test'
 runTests()
 
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => () => (
+  <StrictMode>
+    <App />
+  </StrictMode>
+))
