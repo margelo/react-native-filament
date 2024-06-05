2122ab9154a2f96d3ce8633680d510283fa96189
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-06-05 12:37:26 +0200

    chore: fix example metro bundler using correct RNWC version

--- package/example/Shared/babel.config.js
+++ package/example/Shared/babel.config.js
@@ -1,6 +1,16 @@
 const path = require('path')
 const pak = require('../../package.json')
 
+const aliasMap = {
+  [pak.name]: path.join(__dirname, '..', '..', pak.source),
+  [pak.name + '-test']: path.join(__dirname, '..', '..', pak.test),
+  '~/assets': path.join(__dirname, 'assets'),
+}
+
+if (process.env.IS_NEW_ARCH === 'false') {
+  aliasMap['react-native-worklets-core'] = path.join(__dirname, '..', 'AppExamplePaper', 'node_modules', 'react-native-worklets-core')
+}
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
@@ -8,11 +18,7 @@ module.exports = {
       'module-resolver',
       {
         extensions: ['.tsx', '.ts', '.js', '.json'],
-        alias: {
-          [pak.name]: path.join(__dirname, '..', '..', pak.source),
-          [pak.name + '-test']: path.join(__dirname, '..', '..', pak.test),
-          '~/assets': path.join(__dirname, 'assets'),
-        },
+        alias: aliasMap,
       },
     ],
     ['react-native-reanimated/plugin', { processNestedWorklets: true }],
--- package/example/Shared/index.js
+++ package/example/Shared/index.js
@@ -3,6 +3,9 @@ import App from './src/App'
 import { name as appName } from './app.json'
 import { StrictMode } from 'react'
 
+import { version } from 'react-native-worklets-core/package.json'
+console.log(`Using react-native-worklets-core@${version}`)
+
 // Run filament tests
 import { runTests } from 'react-native-filament-test'
 runTests()
