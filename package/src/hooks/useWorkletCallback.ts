362e071be619e233fd0949af5e394ab86c79b05e
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-06-06 13:37:18 +0200

    fix useWorkletCallback catch errors

--- package/src/hooks/useWorkletCallback.ts
+++ package/src/hooks/useWorkletCallback.ts
@@ -1,5 +1,6 @@
 import { useWorklet } from 'react-native-worklets-core'
 import { useFilamentContext } from '../react/FilamentContext'
+import { reportWorkletError } from '../ErrorUtils'
 
 /**
  * Creates a callback that can be executed in he separate worklet thread of the engine.
@@ -7,6 +8,14 @@ import { useFilamentContext } from '../react/FilamentContext'
 export function useWorkletCallback<T extends (...args: any[]) => any>(callback: T): (...args: Parameters<T>) => Promise<ReturnType<T>> {
   const { workletContext } = useFilamentContext()
 
-  const workletCallback = useWorklet(workletContext, callback)
+  const workletCallback = useWorklet(workletContext, () => {
+    'worklet'
+    try {
+      return callback()
+    } catch (error) {
+      reportWorkletError(error)
+      throw error
+    }
+  })
   return workletCallback
 }
