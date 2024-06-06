226e88ff920f076f28f383c4223a7aeaf08ffa74
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-06-06 15:13:44 +0200

    wip: reize modes?

--- package/example/Shared/src/ImageExample.tsx
+++ package/example/Shared/src/ImageExample.tsx
@@ -21,16 +21,22 @@ type ImageProps = {
   resizeMode?: 'contain' | 'cover'
 }
 
-export function Image({ source }: ImageProps) {
+export function Image({ source, resizeMode = 'contain' }: ImageProps) {
   const { engine, renderableManager, scene, view, transformManager } = useFilamentContext()
 
   // Create the material which will be used to render the image
   const imageMaterialBuffer = useBuffer({ source: ImageMaterial })
   const imageBuffer = useBuffer({ source: source })
+  console.log({
+    imageMaterialBuffer,
+    imageBuffer,
+  })
   const material = useDisposableResource(
     useWorkletCallback(() => {
       'worklet'
+      console.log('herst bist deppert?')
       if (imageMaterialBuffer == null || imageBuffer == null) return undefined
+      console.log('debug 2')
 
       const newMaterial = engine.createMaterial(imageMaterialBuffer)
       const textureInfo = newMaterial.setDefaultTextureParameter(renderableManager, 'image', imageBuffer, 'sRGB')
@@ -46,27 +52,52 @@ export function Image({ source }: ImageProps) {
       const srcRatio = srcWidth / srcHeight
       const dstRatio = dstWidth / dstHeight
 
-      const xMajor = dstWidth / srcWidth > dstHeight / srcHeight
+      let sx: number
+      let sy: number
+      let tx: number
+      let ty: number
 
-      let sx = 1.0
-      let sy = dstRatio / srcRatio
-
-      let tx = 0.0
-      let ty = ((1.0 - sy) * 0.5) / sy
-
-      if (xMajor) {
-        sx = srcRatio / dstRatio
-        sy = 1.0
-        tx = ((1.0 - sx) * 0.5) / sx
-        ty = 0.0
+      if (resizeMode === 'contain') {
+        if (dstRatio > srcRatio) {
+          // Viewport is wider than the image's aspect ratio
+          sx = sy = srcHeight / dstHeight
+          tx = (dstWidth - srcWidth * sy) / (2 * dstWidth)
+          ty = 0
+        } else {
+          // Viewport is narrower than the image's aspect ratio
+          sx = sy = srcWidth / dstWidth
+          tx = 0
+          ty = (dstHeight - srcHeight * sx) / (2 * dstHeight)
+        }
+      } else if (resizeMode === 'cover') {
+        if (dstRatio > srcRatio) {
+          // Viewport is wider than the image's aspect ratio
+          sx = sy = dstWidth / srcWidth
+          tx = 0
+          ty = (dstHeight - srcHeight * sx) / (2 * dstHeight)
+        } else {
+          // Viewport is taller than the image's aspect ratio
+          sx = sy = dstHeight / srcHeight
+          tx = (dstWidth - srcWidth * sy) / (2 * dstWidth)
+          ty = 0
+        }
+      } else {
+        console.log(`Invalid resizeMode: ${resizeMode}`)
+        throw new Error(`Invalid resizeMode: ${resizeMode}`)
       }
 
       // eslint-disable-next-line prettier/prettier
         const transform: Mat3f = [
           1.0 / sx,   0.0,        0.0,
           0.0,        1.0 / sy,   0.0,
-          -tx,        -ty,        1.0
+          -tx,         -ty,        1.0
         ];
+      console.log({
+        sx,
+        sy,
+        tx,
+        ty,
+      })
 
       newMaterial.setDefaultMat3fParameter('transform', transform)
       newMaterial.setDefaultFloat3Parameter('backgroundColor', [0.0, 0.0, 0.0])
@@ -102,7 +133,7 @@ function Renderer() {
       <DefaultLight />
       <Model source={require('~/assets/pengu.glb')} transformToUnitCube />
 
-      <Image source={imageResource} />
+      <Image source={imageResource} resizeMode="cover" />
     </>
   )
 }
