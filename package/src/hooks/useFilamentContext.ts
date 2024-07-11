9ceaee130be6f30664a846a7f1a64752874a1d53
Marc Rousavy <me@mrousavy.com>
2024-07-11 12:47:24 +0200

    Update `useFilamentContext` error message

--- package/src/hooks/useFilamentContext.ts
+++ package/src/hooks/useFilamentContext.ts
@@ -37,9 +37,7 @@ export const FilamentContext = React.createContext<FilamentContextType | undefin
 export function useFilamentContext() {
   const context = React.useContext(FilamentContext)
   if (context === undefined) {
-    throw new Error(
-      'useFilamentContext (and its hooks such as `useScene()`, components like `<Filament />` etc.) must be used within a `<FilamentProvider>` component!'
-    )
+    throw new Error('You tried to use a Filament hook/component without wrapping your component in a <FilamentScene> component!')
   }
   return context
 }
