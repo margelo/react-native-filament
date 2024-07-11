d51ee06763566c8e2c32861585710ab54293ea61
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-07-11 17:14:37 +0200

    add gh actions

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
