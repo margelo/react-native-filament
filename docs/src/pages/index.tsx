7de1fdf30125a4469f5cbf5d86a053d787cc6e89
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-07-15 17:22:28 +0200

    add example

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,29 +1,30 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-
 import styles from './index.module.css';
+
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <header>
+        <iframe src={useBaseUrl('demos/helmet.html')} style={{
+          width: '100%',
+          height: '40vh',
+          zIndex: -1
+        }} />
+
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/guides">
+            Getting started - 5min ⏱️
           </Link>
         </div>
-      </div>
+
     </header>
   );
 }
