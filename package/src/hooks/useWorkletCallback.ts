a86dded9c64d021a787b37e715f94e451f3eeea8
Hanno J. Gödecke <hanno@margelo.io>
2024-07-15 09:40:51 +0200

    fix: `useWorkletCallback` returned unstable ref (#226)

--- package/src/hooks/useWorkletCallback.ts
+++ package/src/hooks/useWorkletCallback.ts
@@ -1,6 +1,7 @@
-import { useWorklet } from 'react-native-worklets-core'
+import { getWorkletDependencies } from 'react-native-worklets-core'
 import { useFilamentContext } from './useFilamentContext'
 import { wrapWithErrorHandler } from '../ErrorUtils'
+import { useMemo } from 'react'
 
 /**
  * Creates a callback that can be executed in he separate worklet thread of the engine.
@@ -8,5 +9,19 @@ import { wrapWithErrorHandler } from '../ErrorUtils'
 export function useWorkletCallback<T extends (...args: any[]) => any>(callback: T): (...args: Parameters<T>) => Promise<ReturnType<T>> {
   const { workletContext } = useFilamentContext()
 
-  return useWorklet(workletContext, wrapWithErrorHandler(callback))
+  // Note: from react-native-worklets-core/useWorklet
+  // As we want to wrap using `wrapWithErrorHandler` the dependencies must be captured from the
+  // callback, not from the wrapper.
+
+  // As a dependency for this use-memo we use all of the values captured inside the worklet,
+  // as well as the unique context name.
+  const dependencies = [...getWorkletDependencies(callback)]
+
+  return useMemo(
+    () => {
+      return workletContext.createRunAsync(wrapWithErrorHandler(callback))
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    dependencies
+  )
 }
