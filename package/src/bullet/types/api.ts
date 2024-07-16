6bcc7bbfaf396cc3ee2d826d78b7c0eff42cf6a9
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-07-16 15:17:55 +0200

    renamed physical shapes to *Shape

--- package/src/bullet/types/api.ts
+++ package/src/bullet/types/api.ts
@@ -1,11 +1,11 @@
 import { Mat4 } from '../../types/TransformManager'
 import { DiscreteDynamicWorld } from './DiscreteDynamicWorld'
 import { CollisionCallback, RigidBody } from './RigidBody'
-import { BaseShape, Box, Cylinder, Sphere, StaticPlane } from './Shapes'
+import { BaseShape, BoxShape, CylinderShape, SphereShape, StaticPlaneShape } from './Shapes'
 
 export interface BulletAPI {
   createDiscreteDynamicWorld(gravityX: number, gravityY: number, gravityZ: number): DiscreteDynamicWorld
-  createBoxShape(halfX: number, halfY: number, halfZ: number): Box
+  createBoxShape(halfX: number, halfY: number, halfZ: number): BoxShape
   /**
    * Implements a cylinder shape primitive, centered around the origin. Its central axis aligned with the Y axis.
    * cylinderShapeX is aligned with the X axis and cylinderShapeZ around the Z axis.
@@ -21,11 +21,11 @@ export interface BulletAPI {
    * @param halfY
    * @param halfZ
    */
-  createCylinderShape(halfX: number, halfY: number, halfZ: number): Cylinder
-  createCylinderShapeX(halfX: number, halfY: number, halfZ: number): Cylinder
-  createCylinderShapeZ(halfX: number, halfY: number, halfZ: number): Cylinder
-  createStaticPlaneShape(normalX: number, normalY: number, normalZ: number, constant: number): StaticPlane
-  createSphereShape(radius: number): Sphere
+  createCylinderShape(halfX: number, halfY: number, halfZ: number): CylinderShape
+  createCylinderShapeX(halfX: number, halfY: number, halfZ: number): CylinderShape
+  createCylinderShapeZ(halfX: number, halfY: number, halfZ: number): CylinderShape
+  createStaticPlaneShape(normalX: number, normalY: number, normalZ: number, constant: number): StaticPlaneShape
+  createSphereShape(radius: number): SphereShape
   createRigidBody(
     mass: number,
     x: number,
