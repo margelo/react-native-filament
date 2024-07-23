6f060b97c1b0ad2911c425d170e1a01ad85fccd2
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-07-23 11:28:49 +0200

    fix: #242 exported worklet functions are undefined

--- package/src/utilities/helper.ts
+++ package/src/utilities/helper.ts
@@ -1,11 +1,11 @@
 import { ISharedValue } from 'react-native-worklets-core'
 import { Float3 } from '../types'
 
-export function areFloat3Equal(a: Float3, b?: Float3): boolean {
+export const areFloat3Equal = (a: Float3, b?: Float3): boolean => {
   return a[0] === b?.[0] && a[1] === b?.[1] && a[2] === b?.[2]
 }
 
-export function isWorkletSharedValue(value: any): value is ISharedValue<any> {
+export const isWorkletSharedValue = (value: any): value is ISharedValue<any> => {
   'worklet'
   return typeof value === 'object' && value != null && 'addListener' in value && typeof value.addListener === 'function'
 }
