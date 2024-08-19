2a2c94e235ad46262e28eea280ed5788af5607c7
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-08-19 11:51:22 +0200

    update nitrogen version

--- package/src/specs/SwapChain.nitro.ts
+++ package/src/specs/SwapChain.nitro.ts
@@ -1,3 +1,6 @@
 import { type HybridObject } from 'react-native-nitro-modules'
 
-export interface SwapChain extends HybridObject<{ android: 'c++'; ios: 'c++' }> {}
+export interface SwapChain extends HybridObject<{ android: 'c++'; ios: 'c++' }> {
+  // TODO: Inheritance is not yet supported by nitrogen
+  release(): void
+}
