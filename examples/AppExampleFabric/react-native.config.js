1e7f6b6ff095cc9a35ef5cd284b24947e89cc623
Hanno J. Gödecke <die.drei99@yahoo.de>
2026-01-08 11:49:17 +0100

    fucking works

--- examples/AppExampleFabric/react-native.config.js
+++
@@ -1,6 +0,0 @@
-const path = require('path')
-const assetPath = path.join(__dirname, '..', 'Shared', 'assets')
-console.log('Linking assets from ', assetPath)
-module.exports = {
-  // assets: [assetPath],
-}
