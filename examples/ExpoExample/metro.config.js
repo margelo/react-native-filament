368c6420c85eed9a0341a067a6b62356cbc7933c
Hanno J. Gödecke <die.drei99@yahoo.de>
2026-01-08 11:49:13 +0100

    fix expo bundler metro config

--- examples/ExpoExample/metro.config.js
+++ examples/ExpoExample/metro.config.js
@@ -4,6 +4,7 @@ const { mergeConfig } = require('@react-native/metro-config');
 
 const path = require('path')
 const pak = require('../../package/package.json')
+const escape = require('escape-string-regexp')
 
 const root = path.resolve(__dirname, '..', '..')
 const modules = Object.keys({ ...pak.peerDependencies })
@@ -21,37 +22,54 @@ fs.readdirSync(assetPath).forEach(file => {
   assetFilesMap[file] = path.join(assetPath, file)
 })
 
+// Project's node modules list
+const nodeModulePath = path.resolve(__dirname, "node_modules")
+const projectNodeModulesMap = {}
+fs.readdirSync(nodeModulePath).forEach(file => {
+  projectNodeModulesMap[file] = path.join(nodeModulePath, file)
+})
+
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = {
   watchFolders: [root],
 
   resolver: {
     assetExts: assetExts,
-    // We need to make sure that only one version is loaded for peerDependencies
-    // So we block them at the root, and alias them to the versions in example's node_modules
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name)
-      return acc
-    }, {
-      'react-native-filament': path.resolve(root, 'package', 'src'),
-      "react-native-filament-test": path.resolve(root, 'package', 'src', 'test', 'RunTests'),
-    }),
+
+    // Block peer dependencies from workspace root to prevent duplicate bundling
+    // Only block modules that exist in the example's node_modules
+    blockList: modules
+      .filter(m => projectNodeModulesMap[m])
+      .map(m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+    ),
     resolveRequest: (context, moduleName, platform) => {
       const baseFileName = path.basename(moduleName);
-      if (!assetFilesMap[baseFileName]) {
-        return context.resolveRequest(context, moduleName, platform);
+
+      // Only handle asset files with custom resolution
+      if (assetFilesMap[baseFileName]) {
+        // The code is in Shared, when bundling the app code the path to our asset would point to "../Shared/assets/{name}"
+        // This would result in a metro server call to: http://server/assets/../Shared/assets/{name}, which will decode to:
+        // http://server/Shared/assets/{name}. Metro expects assets to be requested from /assets/ and thus this would fail.
+        // To fix this I symlink the assets into the example's asset folder and point to that location here.
+        return {
+          type: 'assetFiles',
+          filePaths: [
+            path.resolve(__dirname, 'assets', baseFileName),
+          ]
+        }
       }
-    
-      // The code is in Shared, when bundling the app code the path to our asset would point to "../Shared/assets/{name}"
-      // This would result in a metro server call to: http://server/assets/../Shared/assets/{name}, which will decode to:
-      // http://server/Shared/assets/{name}. Metro expects assets to be requested from /assets/ and thus this would fail.
-      // To fix this I symlink the assets into the example's asset folder and point to that location here.
-      return {
-        type: 'assetFiles',
-        filePaths: [
-          path.resolve(__dirname, 'assets', baseFileName),
-        ]
+
+      // Check if this module should be redirected via extraNodeModules
+      // We need to manually handle this since we have a custom resolveRequest
+      if (projectNodeModulesMap[moduleName]) {
+        console.log(`Resolve module "${moduleName}" => ${projectNodeModulesMap[moduleName]}`)
+        const result = context.resolveRequest(context, projectNodeModulesMap[moduleName], platform);
+        console.log("  "+ JSON.stringify(result))
+        return result
       }
+
+      // For everything else, let Metro's default resolver handle it
+      return context.resolveRequest(context, moduleName, platform);
     }
   },
 };
